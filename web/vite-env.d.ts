48715c51eef304bea7997a38097f04528eeae0f8
GabrielCirqueira <gabrielcirqueira711@gmail.com>
2025-11-28 16:24:40 -0300

    chore(config): adicionar configurações do prettier e commitlint
    - Adicionado arquivo .prettierrc.cjs com configurações do Prettier.
    - Adicionado arquivo commitlint.config.js com regras para mensagens de commit.
    - Atualizado package-lock.json e package.json com novas dependências e versões.

--- web/vite-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '@fontsource/lato'
-declare module '@fontsource/poppins'
-declare module '@fontsource-variable/roboto'
-declare module '@fontsource-variable/inter'
