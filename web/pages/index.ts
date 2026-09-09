26f68ce924a415bc7f44016a55d74ae1b3762113
GabrielCirqueira <gabrielcirqueira711@gmail.com>
2026-09-09 16:33:29 -0300

    refactor: reorganizar barrels e limpar caminhos antigos.

--- web/pages/index.ts
+++
@@ -1,4 +0,0 @@
-export { Component as Home } from './Home/Home'
-export { Component as NotFound } from './NotFound/NotFound'
-export { Component as Login } from './Login/Login'
-export { Component as Cadastro } from './Cadastro/Cadastro'
